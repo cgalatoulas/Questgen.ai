61e3b743c56fe9ca02cfb509e70f9aeaef855bd6
cgalatoulas <87330039+cgalatoulas@users.noreply.github.com>
2022-08-21 17:23:57 +0300

    add local tag

--- Questgen/main.py
+++ Questgen/main.py
@@ -37,7 +37,7 @@ class QGen:
     def __init__(self):
         
         
-        self.tokenizer = T5Tokenizer.from_pretrained('/var/task/t5-base')
+        self.tokenizer = T5Tokenizer.from_pretrained('/var/task/t5-base', local_files_only=True)
         model = T5ForConditionalGeneration.from_pretrained('Parth/result')
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         model.to(device)
@@ -196,7 +196,7 @@ class QGen:
 class BoolQGen:
        
     def __init__(self):
-        self.tokenizer = T5Tokenizer.from_pretrained('/var/task/t5-base')
+        self.tokenizer = T5Tokenizer.from_pretrained('/var/task/t5-base', local_files_only=True)
         model = T5ForConditionalGeneration.from_pretrained('ramsrigouthamg/t5_boolean_questions')
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         model.to(device)
@@ -248,7 +248,7 @@ class BoolQGen:
 class AnswerPredictor:
           
     def __init__(self):
-        self.tokenizer = T5Tokenizer.from_pretrained('/var/task/t5-base')
+        self.tokenizer = T5Tokenizer.from_pretrained('/var/task/t5-base', local_files_only=True)
         model = T5ForConditionalGeneration.from_pretrained('Parth/boolean')
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         model.to(device)
